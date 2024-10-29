e5da28957acc9e3a2058059b8df668866be79ba9
Justin D. <justindejesus323@gmail.com>
2024-10-29 15:57:17 -0400

    word

--- words.c
+++ words.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <dirent.h>
 
+//new and experimenting
+#include <sys/stat.h>
+#include <unistd.h>
+
 //  just place holder for me to remember
 // struct dirent {
 //     ino_t          d_ino;       /* inode number */
@@ -12,6 +16,20 @@
 //                                    by all file system types */
 //     char           d_name[256]; /* filename */
 // };
+
+int isFile(const char* name)
+{
+    DIR* directory = opendir(name);
+
+    if(directory != NULL)
+    {
+     closedir(directory);
+     //is a directory
+     return 0;
+    }
+    //is a file
+    return 1;
+}
 
 void search_directory(const char *dir_path) {
     struct dirent *entry;
@@ -24,22 +42,22 @@ void search_directory(const char *dir_path) {
     while ((entry = readdir(dp)) != NULL) {
         // skip the . and .. entries
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
+            return;
         }
 
         char full_path[1024];
-        
+        //grab the full directory name---------dont know if this works entirely
+        strcat(dir_path, entry->d_name);
         
 
         // check if the entry is a directory
         //for some reason DT_DIR and DT_REG not working for me so might look for another way to do this
-        if (entry->d_type == DT_DIR) {
+        if (isFile(dir_path) == 0) {
             search_directory(full_path);
-        } else if (entry->d_type == DT_REG) {
-            // Check if the file ends with ".txt"
-            if (strstr(entry->d_name, ".txt") != NULL) {
-                // call wordcount or add the path to a place to all execute at once
-            }
+            
+        // Check if the file ends with ".txt"
+        } else if (strstr(dir_path, ".txt") != NULL) {
+            // call wordcount or add the path to a place to all execute at once
         }
     }
 
